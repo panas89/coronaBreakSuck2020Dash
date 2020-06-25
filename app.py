2b125de6208121b7f4b88f1fff46516459b71ff2
pp89@ucla.edu <pp89@ucla.edu>
2020-06-25 07:39:53 -0700

    added publication slide slider, subclass and location dropdown

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import flask
 import dash
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as gobs
+import pandas as pd
+import numpy as np
+import datetime
+
 
 external_stylesheets = ['https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css']
 
@@ -42,11 +47,73 @@ header = dbc.NavbarSimple(
     dark=True
 )
 
+# Data
+# ==========
 
+# read data.
+
+df = pd.read_csv('./data/data.csv',parse_dates=True)
+
+df['publish_time'] = pd.to_datetime(df['publish_time'])
+time_diff = (df['publish_time'].max()-df['publish_time'].min()).days
+print(df.head(3))
+print(str(df['publish_time'].min())[:10])
+print(str(df['publish_time'].min()+ datetime.timedelta(days=1)))
+print((df['publish_time'].max()-df['publish_time'].min()).days)
 # COMPONENTS
 # ==========
 
 # Your components go here.
+
+class_sub_class = html.Div([
+                html.Label('Class - Subclass'),
+                dcc.Dropdown(
+                    id='class-sub-class-drop-down',
+                    options=[
+                        {'label': 'New York City', 'value': 'NYC'},
+                        {'label': 'Montréal', 'value': 'MTL'},
+                        {'label': 'San Francisco', 'value': 'SF'}
+                    ],
+                    value='MTL'
+                )  
+            ], style={'width': '48%',
+                      'padding': '25px 50px 75px 100px',
+                      'display': 'inline-block'})
+
+location = html.Div([
+                html.Label('Location'),
+                dcc.Dropdown(
+                    id='location-drop-down',
+                    options=[
+                        {'label': 'New York City', 'value': 'NYC'},
+                        {'label': 'Montréal', 'value': 'MTL'},
+                        {'label': 'San Francisco', 'value': 'SF'}
+                    ],
+                    value='MTL'
+                )  
+            ], style={'width': '48%',
+                      'padding': '25px 50px 75px 100px',
+                      'display': 'inline-block'})
+
+class_loc = html.Div(
+                    [class_sub_class,location]
+                    , style={'width': '100%',
+                             'display': 'inline-block'})
+
+
+pb_time = html.Div([
+                html.Label('Publication date'),
+                dcc.RangeSlider(
+                    id='pub-date-slider',
+                    min=0,
+                    max=time_diff,
+                    value=[0,10],
+                    marks={time_point : \
+                            str(df['publish_time'].min() \
+                        + datetime.timedelta(days=time_point))[:10] \
+                         for time_point in range(0,time_diff+11,11)},
+                ),
+            ], style={'width': '100%','padding': '25px 50px 75px 100px'})
 
 
 # INTERACTION
@@ -59,7 +126,9 @@ header = dbc.NavbarSimple(
 # ==========
 
 app.layout = html.Div([
-    header
+                        header,
+                        class_loc,
+                        pb_time
 ])
 
 if __name__ == '__main__':
